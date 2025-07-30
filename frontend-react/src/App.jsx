2bbd2762fa61c8e76e4f770f82fbbd1fba124c19
unknown <praveenkpraveen1020@gmail.com>
2025-07-30 17:26:27 +0530

    RegistrationBackend

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -2,13 +2,23 @@ import './assets/css/style.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Main from './components/Main'
+import Register from './components/Register'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 function App() {
 
   return (
     <>
-      <Header/>
-      <Main/>
-      <Footer/>
+
+    <BrowserRouter>
+      <Header />
+    <Routes>
+
+          <Route path='/' element={<Main />} />
+          <Route path='/register' element={<Register />} />
+    </Routes>
+      <Footer />
+    </BrowserRouter>
+
       
     </>
   )
--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,15 +1,16 @@
 
+import { Link } from "react-router-dom"
 import Button from "./Button"
 
 const Header = () =>{
     return(
         <> <nav className="navbar container pt-3 pb-3">
-            <a className="navbar-brand text-light"  href="#">Stock Prediction</a>
+            <Link className="navbar-brand text-light"  to={'/'}>Stock Prediction</Link>
 
             <div>
-                <Button text='Login' class='btn-info'/>
+                <Button text='Login' class='btn-info' url="/login"/>
                 &nbsp;
-                <Button text='Register' class='btn-outline-info'/>
+                <Button text='Register' class='btn-outline-info' url="/register" />
             </div>
         </nav>
 
